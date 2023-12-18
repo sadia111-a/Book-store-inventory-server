9a711794da29fe2ef11d88d1d77f1b56f90dd2db
sadia11-a <sadiasorna96@gmail.com>
2023-12-18 22:50:20 +0600

    get the book data by specific id

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t3yv0bn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +26,59 @@ async function run() {
     await client.connect();
 
     const bookCollection = client.db("bookDb").collection("book");
+
+    // get all book from database
+    app.get("/all-book", async (req, res) => {
+      const books = await bookCollection.find().toArray();
+      res.send(books);
+    });
+    // insert a book to the database
+    app.post("/upload-book", async (req, res) => {
+      const data = req.body;
+      const result = await bookCollection.insertOne(data);
+      res.send(result);
+    });
+    // update a book
+    app.patch("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateBookData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...updateBookData,
+        },
+      };
+      // update
+      const result = await bookCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    // delete book data
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    // find book by category
+    app.get("/all-book", async (req, res) => {
+      let query = {};
+      if (req.query?.category) {
+        query = { category: req.query.category };
+      }
+      const result = await bookCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // to get single book
+    app.get("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollection.findOne(filter);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
